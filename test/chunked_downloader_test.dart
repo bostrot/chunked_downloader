fa46edf4479f375e9e43e34d8eb7dfdd53e38962
Eric Trenkel <eric@bostrot.com>
2022-12-03 22:15:15 +0100

    Init commit

---
+++ test/chunked_downloader_test.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:chunked_downloader/chunked_downloader.dart';
+
+void main() {
+  String img = 'https://storage.googleapis.com/cms-storage-bucket'
+      '/a40ceb6e5d342207de7b.png';
+  test('downloads a file to a location', () async {
+    bool done = false;
+    ChunkedDownloader(
+        url: img,
+        savedDir: '/tmp',
+        fileName: 'flutter_image.png',
+        onProgress: (received, total, speed) {},
+        onDone: (file) {
+          done = true;
+        }).start();
+    // Wait
+    while (!done) {
+      await Future.delayed(const Duration(milliseconds: 100));
+    }
+    expect(File('/tmp/flutter_image.png').existsSync(), true);
+  });
+}
